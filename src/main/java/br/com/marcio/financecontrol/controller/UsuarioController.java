9b188bbdc496d166159002141c343df5273bf13f
MarcioMAlemida <marcioa.marcioa@gmail.com>
2025-09-26 20:43:26 -0300

    feat(usuario): Expõe endpoint para criação de novos usuários

---
+++ src/main/java/br/com/marcio/financecontrol/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package br.com.marcio.financecontrol.controller;
+
+import br.com.marcio.financecontrol.dto.UsuarioRequestDTO;
+import br.com.marcio.financecontrol.dto.UsuarioResponseDTO;
+import br.com.marcio.financecontrol.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> criarUsuario(@Valid @RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        UsuarioResponseDTO novoUsuario = usuarioService.criarUsuario(usuarioRequestDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(novoUsuario.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(novoUsuario);
+    }
+}
